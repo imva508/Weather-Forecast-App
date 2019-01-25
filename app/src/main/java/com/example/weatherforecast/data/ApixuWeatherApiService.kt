4ae76e69896336dff03e2a2d4a3c457b5226e77f
Varun Sharma <imva508@gmail.com>
2019-01-25 19:30:11 +0530

    Created the weather response objects

---
+++ app/src/main/java/com/example/weatherforecast/data/ApixuWeatherApiService.kt
@@ -0,0 +1,9 @@
+package com.example.weatherforecast.data
+
+const val API_KEY = "18fe8a99644c453fb47133635192501"
+
+// http://api.apixu.com/v1/current.json?key=18fe8a99644c453fb47133635192501&q=Paris
+
+interface ApixuWeatherApiService {
+
+}
