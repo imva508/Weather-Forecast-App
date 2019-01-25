5bf46a4f7d9064342606f2c1c56aebdb431cff7b
Varun Sharma <imva508@gmail.com>
2019-01-25 19:39:18 +0530

    Added git invoke operator

--- app/src/main/java/com/example/weatherforecast/data/ApixuWeatherApiService.kt
+++ app/src/main/java/com/example/weatherforecast/data/ApixuWeatherApiService.kt
@@ -1,9 +1,28 @@
 package com.example.weatherforecast.data
 
+import com.example.weatherforecast.data.response.CurrentWeatherResponse
+import kotlinx.coroutines.Deferred
+import org.intellij.lang.annotations.Language
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 const val API_KEY = "18fe8a99644c453fb47133635192501"
 
 // http://api.apixu.com/v1/current.json?key=18fe8a99644c453fb47133635192501&q=Paris
 
 interface ApixuWeatherApiService {
 
+    @GET("current.json")
+    fun getCurrentWeather(
+        @Query("q") location: String,
+        @Query("lang") language: String = "en"
+    ): Deferred<CurrentWeatherResponse>
+
+
+    companion object {
+
+        operator fun invoke():ApixuWeatherApiService{
+
+        }
+    }
 }
