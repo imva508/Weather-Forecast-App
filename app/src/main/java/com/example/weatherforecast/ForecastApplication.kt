746867b9eab5076b4bf68623dc0333f3f3a041fb
Varun Sharma <imva508@gmail.com>
2019-01-26 13:06:47 +0530

    Added dependency injection with kodein(Kotlin)

--- app/src/main/java/com/example/weatherforecast/ForecastApplication.kt
+++ app/src/main/java/com/example/weatherforecast/ForecastApplication.kt
@@ -1,5 +1,28 @@
 package com.example.weatherforecast
 
 import android.app.Application
+import com.example.weatherforecast.data.db.CurrentWeatherDao
+import com.example.weatherforecast.data.db.ForecastDatabase
+import com.example.weatherforecast.data.db.repository.ForecastRepository
+import com.example.weatherforecast.data.db.repository.ForecastRepositoryImpl
+import com.example.weatherforecast.data.network.*
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
 
-class ForecastApplication:Application()
+class ForecastApplication : Application(), KodeinAware {
+
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@ForecastApplication))
+
+        bind() from singleton { ForecastDatabase(instance()) }
+        bind() from singleton { instance<ForecastDatabase>().currentWeatherDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { ApixuWeatherApiService(instance()) }
+        bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
+    }
+}
